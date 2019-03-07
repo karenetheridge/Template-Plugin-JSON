d567b70b8516bd2a833a1aceb88365e7452ffd33
Karen Etheridge <ether@cpan.org>
2019-03-07 13:40:53 -0800

    convert packaging to Dist::Zilla

--- lib/Template/Plugin/JSON.pm
+++ lib/Template/Plugin/JSON.pm
@@ -3,6 +3,8 @@
 use 5.006;
 
 package Template::Plugin::JSON;
+# ABSTRACT: Adds a .json vmethod for all TT values.
+
 use Moose;
 
 use JSON ();
@@ -85,10 +87,6 @@ __END__
 
 =pod
 
-=head1 NAME
-
-Template::Plugin::JSON - Adds a .json vmethod for all TT values.
-
 =head1 SYNOPSIS
 
 	[% USE JSON ( pretty => 1 ) %];
@@ -119,35 +117,4 @@ Any options on the USE line are passed through to the JSON object, much like L<J
 
 L<JSON>, L<Template::Plugin>
 
-=head1 VERSION CONTROL
-
-L<https://github.com/neilb/Template-Plugin-JSON>
-
-=head1 AUTHOR
-
-Yuval Kogman <nothingmuch@woobling.org>
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright (c) 2006, 2008 Infinity Interactive, Yuval Kogman.
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-DEALINGS IN THE SOFTWARE.
-
 =cut
-
